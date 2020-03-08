682a68d5164a4ed1495c39f5f632d8b98c29e988
jiangweixian <jiangweixian1994@gmail.com>
2020-03-08 18:13:29 +0800

    feat(react): hybrid back in react way

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,3 @@
 export { useAsyncState } from './use-async-state'
 export { useClickOutside } from './use-clickoutside'
+export { useHybridBack } from './use-hybrid-back'
---
+++ packages/react/src/use-hybrid-back.ts
@@ -0,0 +1,21 @@
+import { useState, useCallback, useRef } from 'react'
+
+export const useHybridBack = (closeSchema = '') => {
+  const [herf, setHerf] = useState(window.location.href)
+  const clock = useRef<number>()
+  const handleBack = useCallback(() => {
+    clearTimeout(clock.current)
+    window.history.go(-1)
+    clock.current = window.setTimeout(() => {
+      if (window.location.href === herf) {
+        window.location.href == closeSchema
+        return
+      }
+      setHerf(window.location.href)
+      clearTimeout(clock.current)
+    }, 300)
+  }, [herf, closeSchema])
+  return {
+    handleBack,
+  }
+}
