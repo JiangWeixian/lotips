61b85b8d2181b30a2cbe576a5f9649a3174c2e80
jiangweixian <jiangweixian1994@gmail.com>
2020-03-08 20:25:17 +0800

    feat(react): get components of a or not of a

---
+++ packages/react/src/get-components-of-a.ts
@@ -0,0 +1,8 @@
+import React from 'react'
+import { isComponentOfA } from './is-component-of-a'
+
+export const getComponentsOfA = (children: React.ReactNode, target: React.ComponentType<any>) => {
+  return React.Children.map(children, child => {
+    return isComponentOfA(child, target) ? child : null
+  })
+}
--- packages/react/src/use-hybrid-back.ts
+++ packages/react/src/use-hybrid-back.ts
@@ -1,3 +1,7 @@
+/**
+ *
+ */
+
 import { useState, useCallback, useRef } from 'react'
 
 export const useHybridBack = (closeSchema = '') => {
