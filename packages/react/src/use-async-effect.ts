313a99520373ba7f61494e7a9e534b9c9179df36
JiangWeixian <saber_wei@163.com>
2020-03-10 17:07:51 +0800

    test(react): use async effect

--- packages/react/src/use-async-effect.ts
+++ packages/react/src/use-async-effect.ts
@@ -1,14 +1,17 @@
 import { useEffect } from 'react'
 
-const EMPTY_DEPS = []
+const EMPTY_DEPS: any[] = []
 
-export const useAsyncEffect = (callback: () => Promise<Function | undefined>, deps?: any[]) => {
+export const useAsyncEffect = (
+  callback: () => Promise<Function | undefined | void>,
+  deps?: any[],
+) => {
   useEffect(() => {
-    let result: any
+    let result: Function | undefined | void
     const asyncEffect = async () => {
       result = await callback()
     }
     asyncEffect()
-    return () => result?.()
+    return () => result && result()
   }, [(deps ?? EMPTY_DEPS).concat([callback])])
 }
---
+++ packages/react/test/use-async-effect.test.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useContext } from 'react'
+import { renderHook, act } from '@testing-library/react-hooks'
+import { useAsyncEffect } from '../src/use-async-effect'
+import { delay } from '../utils/delay'
+
+const AsyncEffectContext = React.createContext(1)
+
+describe('use-async-effect', () => {
+  test('work with async function', async () => {
+    const _useAsyncEffect = () => {
+      const value = useContext(AsyncEffectContext)
+      return {
+        value,
+      }
+    }
+    const AsyncEffectProvider = ({ children }) => {
+      const [value, setValue] = useState(1)
+      useAsyncEffect(async () => {
+        await delay(1000)
+        setValue(2)
+      }, [])
+      return <AsyncEffectContext.Provider value={value}>{children}</AsyncEffectContext.Provider>
+    }
+    const hook = renderHook(() => _useAsyncEffect(), { wrapper: AsyncEffectProvider })
+    await hook.waitForNextUpdate()
+    expect(hook.result.current.value).toBe(2)
+  })
+
+  test('clear function shoud work fine', async () => {
+    let reseted = 0
+    const _useAsyncEffect = () => {
+      const [value, setValue] = useState()
+      useAsyncEffect(async () => {
+        return () => {
+          reseted += 1
+        }
+      }, [])
+      return {
+        value,
+        setValue,
+      }
+    }
+    const { unmount } = renderHook(() => _useAsyncEffect())
+    await delay(2000)
+    act(() => {
+      unmount()
+    })
+    expect(reseted).toBe(1)
+  }, 5000)
+})
