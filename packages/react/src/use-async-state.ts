c00d002e3a37218b6ab610560d23e2abe9027366
JiangWeixian <saber_wei@163.com>
2020-03-11 11:00:14 +0800

    docs(react): use-async-state

--- packages/react/src/use-async-state.ts
+++ packages/react/src/use-async-state.ts
@@ -8,8 +8,10 @@
  *     setState(data)
  *  }
  * }, [])
- * 但有时数据是来自redux的异步获取，在以下假设下该hook比较实用
- * - 没有相应的state action，或者不需要相应的state action
+ * NOTE:
+ * - 但有时数据是来自redux的异步获取，在以下假设下该hook比较实用。没有相应的state action，或者不需要相应的state action
+ * WARNING:
+ * - 适合对同一个ID的对象进行修改。在`Form`中的涉及到重复的初始化的情况，不太合适`useAsyncState`
  * ```
  */
 
