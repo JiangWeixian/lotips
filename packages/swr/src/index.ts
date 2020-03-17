a8d0651aca95fd750fb7488cdf21229488a4c58e
JiangWeixian <saber_wei@163.com>
2020-03-17 11:14:18 +0800

    chore(swr): examport

--- packages/swr/src/index.ts
+++ packages/swr/src/index.ts
@@ -1,8 +1,1 @@
-import ms from 'ms'
-import lunchtime from './lunchtime.js'
-import millisecondsUntil from './millisecondsUntil.js'
-
-export default function howLongUntilLunch(hours: number = 12, minutes: number = 30): string {
-  const millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes))
-  return ms(millisecondsUntilLunchTime, { long: true })
-}
+export { usePage } from './use-page'
