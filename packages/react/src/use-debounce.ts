a91482f9055c03521e04f8142e553ed8907047ee
JiangWeixian <saber_wei@163.com>
2020-03-18 11:57:02 +0800

    test(use-debounced): test passed

--- packages/react/src/use-debounce.ts
+++ packages/react/src/use-debounce.ts
@@ -4,9 +4,9 @@
  * USAGE:
  * 正常使用setValue即可
  * NOTE:
- * （以filter为例）
+ * （以修改filter为例）
  * - debouncedValue - 是最后生效的数据，用于数据请求
- * - value - 实际修改的数据，用户数据展示
+ * - value - 实际修改的数据，用于数据展示
  */
 
 import { useState, useEffect } from 'react'
---
+++ packages/react/test/use-debounce.test.tsx
@@ -0,0 +1,27 @@
+import { renderHook, act } from '@testing-library/react-hooks'
+import { useDebounce } from '../src/use-debounce'
+import { delay } from '../utils/delay'
+
+describe('use-debounce', () => {
+  test('default value should work', () => {
+    const { result } = renderHook(() => useDebounce({ defaultValue: 1 }))
+    expect(result.current.value).toBe(1)
+    expect(result.current.debouncedValue).toBe(1)
+  })
+
+  test('value should be react, debounced value should be debounced', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useDebounce({ interval: 2000 }))
+    act(() => {
+      Array(10)
+        .fill(0)
+        .forEach(v => {
+          result.current.setValue(2)
+        })
+    })
+    await delay(1000)
+    expect(result.current.debouncedValue).toBe(undefined)
+    await delay(2000)
+    expect(result.current.value).toBe(2)
+    expect(result.current.debouncedValue).toBe(2)
+  }, 5000)
+})
