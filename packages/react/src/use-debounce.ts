ed1a77267e9d550cee3771fd291f7382c8a57638
JiangWeixian <saber_wei@163.com>
2020-03-18 11:42:07 +0800

    feat(react): debounced hooks

---
+++ packages/react/src/use-debounce.ts
@@ -0,0 +1,35 @@
+/**
+ * WHY:
+ * a debounced hooks
+ * USAGE:
+ * 正常使用setValue即可
+ * NOTE:
+ * （以filter为例）
+ * - debouncedValue - 是最后生效的数据，用于数据请求
+ * - value - 实际修改的数据，用户数据展示
+ */
+
+import { useState, useEffect } from 'react'
+
+type UseDebounceProps<T = any> = {
+  interval?: number
+  defaultValue?: T
+}
+
+export const useDebounce = <T>({ interval, ...props }: UseDebounceProps<T>) => {
+  const [value, setValue] = useState<T | undefined>(props.defaultValue)
+  const [debouncedValue, setDebouncedValue] = useState<T | undefined>(props.defaultValue)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value)
+    }, interval)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [value, interval])
+  return {
+    value,
+    debouncedValue,
+    setValue,
+  }
+}
