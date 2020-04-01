843793b52dbdf4304629fb0ce1c9344f1892f857
JiangWeixian <saber_wei@163.com>
2020-04-01 11:58:36 +0800

    ✨ (react): use vh

---
+++ packages/react/src/use-vh.ts
@@ -0,0 +1,21 @@
+/**
+ * WHY:
+ * refs: https://stackoverflow.com/questions/37112218/css3-100vh-not-constant-in-mobile-browser
+ * NOTE:
+ * if window resize, should exec setRealHeight again
+ */
+
+import { useEffect, useCallback } from 'react'
+
+export const useVH = () => {
+  const setRealHeight = useCallback(() => {
+    const h = window.innerHeight
+    document.documentElement.style.setProperty('--real-vh', `${h * 0.01}`)
+  }, [])
+  useEffect(() => {
+    setRealHeight()
+  }, [])
+  return {
+    setRealHeight,
+  }
+}
