6b2632f28a3e7db48b41fc9320c8d1a939c11e6c
jiangweixian <jiangweixian1994@gmail.com>
2020-05-03 01:38:09 +0800

    🐛 (internal): rollup will earse empty condition code

--- packages/next/src/use-router-record.ts
+++ packages/next/src/use-router-record.ts
@@ -4,22 +4,19 @@
  * WARNING:
  * Routes is not a react.state
  */
-// import { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import { record } from '../../internal/route-record'
+export { Routes } from '../../internal/route-record'
 
 let inited = false
 
-// export const useRouterRecord = () => {
-//   const router = useRouter()
-//   if (!inited && router.events) {
-//     record({ path: router.pathname, name: router.asPath })
-//     router.events.on('routeChangeComplete', path => {
-//       inited = true
-//       record({ path, name: path })
-//     })
-//   }
-// }
-
-console.log(inited, record)
-
-export const useRouterRecord = 1
+export const useRouterRecord = () => {
+  const router = useRouter()
+  if (!inited && router.events) {
+    record({ path: router.pathname, name: router.asPath })
+    router.events.on('routeChangeComplete', path => {
+      inited = true
+      record({ path, name: path }, false)
+    })
+  }
+}
