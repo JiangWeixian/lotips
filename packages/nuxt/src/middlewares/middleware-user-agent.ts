9cff9c11d31a992291b2251fcb99c523faa0bf21
JiangWeixian <saber_wei@163.com>
2020-05-15 16:47:14 +0800

    📝 (nuxt): why ua middleware

--- packages/nuxt/src/middlewares/middleware-user-agent.ts
+++ packages/nuxt/src/middlewares/middleware-user-agent.ts
@@ -1,5 +1,7 @@
 /**
  * refs: https://zh.nuxtjs.org/api/configuration-router/#middleware
+ * WHY:
+ * 如果在`mounted`阶段通过`ua`设置一些东西，那么对于`ssr`会有闪屏发生
  * WARNING:
  * 只能够在`page`的`asyncData`拿到`context.ua`。而在非页面组件组件中是[无法使用asyncData](https://zh.nuxtjs.org/faq/async-data-components/)。
  */
